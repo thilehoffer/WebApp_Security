1358fbbb15ca1468728c1ee3181fde5e8c7dd2e7
toddh <toddh@DESKTOP-4TF4RTU>
2021-11-02 11:49:25 -0400

    Add project files.

---
+++ WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp_UnderTheHood.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        public Credential Credential { get; set; }
+        public void OnGet()
+        {
+
+        }
+    }
+    public class Credential {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
