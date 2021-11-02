aede224b9d03786e5e5ed5d295d0f38b59e38081
Todd Hilehoffer <toddhilehoffer@yahoo.com>
2021-11-02 12:07:08 -0400

    Part 5: creating login page

--- WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
+++ WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -10,14 +10,19 @@ namespace WebApp_UnderTheHood.Pages.Account
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
         public Credential Credential { get; set; }
         public void OnGet()
         {
-
+            this.Credential = new Credential { UserName = "admin" };
+        }
+        public void OnPost() { 
+        
         }
     }
     public class Credential {
         [Required]
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
         [Required]
         [DataType(DataType.Password)]
