01e3cf2f68a6d414f4cedd40af1c93e189720032
Todd Hilehoffer <toddhilehoffer@yahoo.com>
2021-11-09 15:00:38 -0500

    chapter 10 logout page

---
+++ WebApp_UnderTheHood/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace WebApp_UnderTheHood.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnPostAsync () 
+        {
+            await HttpContext.SignOutAsync("MyCookieAuth");
+            return RedirectToAction("Index");
+
+        }
+    }
+}
